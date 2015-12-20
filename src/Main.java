06fe2e31f54f3b6bcccfeefee71a51a87e9d5bc1
Ivan Prymak <esquilo1994@gmail.com>
2015-12-20 23:02:40 +0200

    GUI. Algorithm remastered

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,76 @@
-import java.util.HashSet;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
-    private static final int iterations =300;
-    private static final HashSet<Integer> N= new HashSet<>();
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main extends Application {
+    private static final int ITERATIONS = 10000;
+    private static final HashMap<Integer, XYChart.Series> N = new HashMap<>();
 
     public static void main(String[] args) {
-        N.add(10);
-        N.add(15);
-        N.add(20);
-        N.add(25);
-        N.add(30);
-
-        int sizeNum;
-        while (true) {
-            System.out.println("Enter matrix size from the set {10, 15, 20, 25, 30}:");
-            Scanner in = new Scanner(System.in);
-            sizeNum = in.nextInt();
-            if (!N.contains(sizeNum)){
-                System.out.print("Error in matrix size\n");
-            }else{break;}
-        }
-
-        for (int i = 1; i <= 100; i++) {
-            int conflictNumber = 0;
-            for (int j = 0; j < iterations; j++) {
-                Matrix matrix = new Matrix(sizeNum, i);
-                if (matrix.hasConflict()) {
-                    conflictNumber++;
-                }
-            }
-            System.out.println(String.format("%d. Number of conflict assignments: %d. Per: %.1f%%", i, conflictNumber, (float) conflictNumber / iterations * 100));
-        }
-
+        launch(args);
     }
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        N.put(10, new XYChart.Series());
+        N.put(15, new XYChart.Series());
+        N.put(20, new XYChart.Series());
+        N.put(25, new XYChart.Series());
+        N.put(30, new XYChart.Series());
+
+        CurvedFittedAreaChart chart = new CurvedFittedAreaChart(
+                new NumberAxis("filling, %", 0, 100, 10), new NumberAxis("probability, %",0, 100, 10));
+        chart.setLegendVisible(false);
+        chart.setHorizontalGridLinesVisible(false);
+        chart.setVerticalGridLinesVisible(false);
+        chart.setAlternativeColumnFillVisible(false);
+        chart.setAlternativeRowFillVisible(false);
+        N.entrySet().forEach(n -> {
+            for (int i = 0; i <= 100; i++) {
+                int conflictNumber = 0;
+                for (int j = 0; j < ITERATIONS; j++) {
+                    Matrix matrix = new Matrix(n.getKey(), i);
+                    if (matrix.hasConflict()) {
+                        conflictNumber++;
+                    }
+                }
+                n.getValue().getData().add(new XYChart.Data<>(i, ((float)conflictNumber/ITERATIONS)*100));
+            }
+        });
+
+        VBox root = new VBox(5d);
+        ToggleGroup sizesGroup = new ToggleGroup();
+        List<ToggleButton> buttons = N.entrySet().stream().sorted((o1, o2) -> Integer.compare(o1.getKey(), o2.getKey())).map(n -> {
+            ToggleButton button = new ToggleButton(String.valueOf(n.getKey()));
+            button.setToggleGroup(sizesGroup);
+            button.setUserData(n.getKey());
+            return button;
+        }).collect(Collectors.toList());
+        sizesGroup.selectedToggleProperty().addListener((ov, toggle, newToggle)->{
+            chart.getData().clear();
+            chart.getData().add(N.get(sizesGroup.getSelectedToggle().getUserData()));
+        });
+        sizesGroup.getToggles().get(0).setSelected(true);
+        HBox buttonHolder = new HBox();
+        buttonHolder.getStyleClass().add("hbox");
+        buttonHolder.setFillHeight(false);
+        buttons.forEach(b->buttonHolder.getChildren().add(b));
+        root.getChildren().addAll(chart, buttonHolder);
+        VBox.setVgrow(chart, Priority.ALWAYS);
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(("CurveFittedChart.css"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,7 +1,8 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Matrix {
-    int[][] matrix;
+    ArrayList<ArrayList<Integer>> rows;
     int amountFull;
     int size;
     boolean hasConflict;
@@ -17,45 +18,84 @@ public class Matrix {
             }
         }
 
-        this.matrix = new int[size][size];
-        this.size = size;
-        this.amountFull = (int) (((float) (size * size) / 100) * filling);
-
+        this.rows = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
+            ArrayList<Integer> row = new ArrayList<>(size);
             for (int j = 0; j < size; j++) {
-                matrix[i][j] = 0;
+                row.add(0);
             }
+            rows.add(row);
         }
 
-        FillMatrix();
+        this.size = size;
+        this.amountFull = (int) (((float) (size * size) / 100) * filling);
+        fill();
+        stripExplicit();
         testConflict();
     }
 
 
-    private void FillMatrix() {
+    private void fill() {
         Random rand = new Random();
         int candidate;
         for (int i = 0; i < amountFull; i++) {
             Position candidatePos;
             candidate = rand.nextInt(candidates.size());
             candidatePos = candidates.get(candidate);
-            matrix[candidatePos.getI()][candidatePos.getJ()] = 1;
+            set(candidatePos.getI(), candidatePos.getJ(), 1);
             candidates.remove(candidatePos);
         }
     }
 
-    public void testConflict() {
-        transformMatrix();
-        hasConflict =true;
-        for (int i = 0; i < size/2 ; i++) {
-            for (int j =size/2; j <size ; j++) {
-                if (matrix[i][j]!=0){
-                    hasConflict =false;
+    private void stripExplicit() {
+        boolean flag;
+        int lastI = 0, lastJ = 0;
+        do {
+            flag = false;
+            for (int i = 0; i < size; i++) {
+                int counter = 0;
+                for (int j = 0; j < size; j++) {
+                    if (get(i, j) == 1) {
+                        counter++;
+                        lastJ = j;
+                    }
+                }
+                if (counter == 1) {
+                    delete(i, lastJ);
+                    flag = true;
                 }
             }
 
-        }
+            for (int j = 0; j < size; j++) {
+                int counter = 0;
+                for (int i = 0; i < size; i++) {
+                    if (get(i, j) == 1) {
+                        counter++;
+                        lastI = i;
+                    }
+                }
+                if (counter == 1) {
+                    delete(lastI, j);
+                    flag = true;
+                }
+            }
+        } while (flag);
+    }
 
+    public void testConflict() {
+        transformMatrix();
+        hasConflict = false;
+        for (int k = 1; k < size - 1; k++) {
+            if (get(k, k) == 1) {
+                for (int i = 0; i <= k; i++) {
+                    for (int j = k; j < size; j++) {
+                        if (get(i, j) == 1 && (i != k || j != k))
+                            return;
+                    }
+                }
+                hasConflict = true;
+            }
+        }
     }
 
     public boolean hasConflict() {
@@ -65,7 +105,7 @@ public class Matrix {
     public void show() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.print(matrix[i][j] + "  ");
+                System.out.print(get(i, j) + "  ");
             }
             System.out.print("\n");
         }
@@ -83,7 +123,7 @@ public class Matrix {
             for (int i = k; i < size; i++) {
                 tempj[i] = 0;
                 for (int j = k; j < size; j++) {
-                    tempj[i] = tempj[i] + matrix[i][j];
+                    tempj[i] = tempj[i] + get(i, j);
                 }
                 if (tempj[Mini] > tempj[i]) {
                     Mini = i;
@@ -94,9 +134,9 @@ public class Matrix {
             //Step2 change MinLine with first
             int temp1;
             for (int j = 0; j < size; j++) {
-                temp1 = matrix[Mini][j];
-                matrix[Mini][j] = matrix[k][j];
-                matrix[k][j] = temp1;
+                temp1 = get(Mini, j);
+                set(Mini, j, get(k, j));
+                set(k, j, temp1);
             }
             //System.out.println(String.format("Swaped rows %d with %d", k, Mini));
 
@@ -105,9 +145,9 @@ public class Matrix {
             int Maxj = k;
             for (int j = k; j < size; j++) {
                 tempi[j] = 0;
-                if (matrix[k][j] == 1) {
+                if (get(k, j) == 1) {
                     for (int i = k; i < size; i++) {
-                        tempi[j] = tempi[j] + matrix[i][j];
+                        tempi[j] = tempi[j] + get(i, j);
                     }
                     if (tempi[j] > tempi[Maxj]) {
                         Maxj = j;
@@ -118,13 +158,27 @@ public class Matrix {
             //Step 4 change MaxColumn with first
             int temp2;
             for (int i = 0; i < size; i++) {
-                temp2 = matrix[i][Maxj];
-                matrix[i][Maxj] = matrix[i][k];
-                matrix[i][k] = temp2;
+                temp2 = get(i, Maxj);
+                set(i, Maxj, get(i, k));
+                set(i, k, temp2);
             }
             //System.out.println(String.format("Swaped columns %d with %d", k, Maxj));
 
         }
 
+    }
+
+    public int get(int i, int j) {
+        return rows.get(i).get(j);
+    }
+
+    public void set(int i, int j, int elem) {
+        rows.get(i).set(j, elem);
+    }
+
+    public void delete(int i, int j) {
+        rows.remove(i);
+        rows.forEach(row->row.remove(j));
+        size--;
     }
 }
